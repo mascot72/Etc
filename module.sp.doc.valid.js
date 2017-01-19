72941897fffde4380127f3c7496736d42515437f
Isaac <joyhobby@hotmail.com>
2017-01-19 11:26:48 +0900

    comment

--- module.sp.doc.valid.js
+++ module.sp.doc.valid.js
@@ -78,5 +78,27 @@ var formV = new validToDoc({
     }
 });
 
+
 //Equip Update before
 //Request before
+
+
+    //Ajax Bloking회피하기위해 Interval처리
+    myVar = setInterval(function(){ initTimer() }, 500);    //0.5초마다
+    var myVar;  //tiemer flag Object
+	var excutedCnt = 0; //실행된 수
+    var isSavedStage = false;   //Header저장종료여부
+	function stopInitTimer() {
+	    clearInterval(myVar);
+	}	
+	function initTimer(){
+		if (excutedCnt > 400){	//timeout:200,000msec : 무한기다림 회피
+			stopInitTimer();
+		} else if (isSavedStage){
+			stopInitTimer();	//Timer소멸
+			isSavedStage = false;
+			//call PDF Popup
+            openPackagePDF();            
+		}		
+		excutedCnt++;
+	}
